c5b9549dd2e29268f9a4e5bbaa3bff9b15e800fe
Siddharth Bhat <siddu.druid@gmail.com>
2022-06-15 19:12:07 +0100

    Add some explanation about the parser infrastructure.

--- CParser/Parser/GroupOne.lean
+++ CParser/Parser/GroupOne.lean
@@ -4,9 +4,17 @@ import CParser.Util
 open AST
 
 
+-- Parse a piece of lean Syntax into a PrimaryExpr.
 partial def mkPrimaryExpression : Lean.Syntax → Except String PrimaryExpr
   | `(primary_expression| $s:ident) => return (PrimaryExpr.Identifier s.getId.toString)
   | _ => throw "unexpected syntax"
 
-def parsePrimaryExpression := mkNonTerminalParser `primary_expression mkPrimaryExpression
-def parse := parsePrimaryExpression
+-- Parse a primary expression into 
+def parsePrimaryExpression : String → Lean.Environment → Option String × PrimaryExpr := 
+  mkNonTerminalParser `primary_expression mkPrimaryExpression
+
+-- Parse the top-level nonterminal of our grammar.
+def parseToplevelNonterminal := parsePrimaryExpression
+
+-- C parser, which invokes the top level nonterminal parser.
+def parse := parseToplevelNonterminal
--- CParser/Util.lean
+++ CParser/Util.lean
@@ -5,6 +5,10 @@ open Lean
 
 -- for now: just single-line comments
 -- TODO: this also isn't good enough, it will break if there's a string literal with // inside it, and it shouldn't
+-- bollu:
+--   Is this correct? This only removes LEADING comments. It does not seem to remove something like:
+--   abb // cc
+--   ?
 def removeCommentsLineCharList : List Char → List Char
 | c1::c2::cs => if c1 == '/' && c2 == '/' then [] else c1::(removeCommentsLineCharList (c2::cs))
 | l => l
@@ -21,6 +25,8 @@ private def mkParseFun {α : Type} (syntaxcat : Name) (ntparser : Syntax → Exc
 String → Environment → Except String α := λ s env => do
   ntparser (← Parser.runParserCategory env syntaxcat s)
 
+-- Create a parser for a syntax category named `syntaxcat`, which uses `ntparser` to read a syntax node and produces a value α, or an error.
+-- This returns a function that given a string `s` and an environment `env`, tries to parse the string, and produces an error.
 def mkNonTerminalParser {α : Type} [Inhabited α] (syntaxcat : Name) (ntparser : Syntax → Except String α)
 (s : String) (env : Environment) : Option String × α :=
   let parseFun := mkParseFun syntaxcat ntparser
