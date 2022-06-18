952b5a128801a075d61058f51b17d5b02818d9b5
Abhinav271828 <abhinav.menon@students.iiit.ac.in>
2022-06-18 15:35:02 +0530

    Add regex matcher

--- CParser/Util.lean
+++ CParser/Util.lean
@@ -33,3 +33,45 @@ def mkNonTerminalParser {α : Type} [Inhabited α] (syntaxcat : Name) (ntparser 
   match parseFun s env with
    | .error msg => (some msg, default)
    | .ok    p   => (none, p)
+
+-- For regex matching
+inductive Regex where
+  | Concat : List Regex → Regex
+  | Union : List Regex → Regex
+  | Star : Regex → Regex
+  | Base : Char → Regex
+
+open Regex
+def qmark (r : Regex) : Regex := Union [empty, r]
+def plus  (r : Regex) : Regex := Concat [r, Star r]
+
+def empty : Regex := Concat []
+def d : Regex := Union (List.map Base ['0','1','2','3','4','5','6','7','8','9'])
+def l : Regex := Union (List.map Base ['a','b','c','d','e','f','g','h',
+                                       'i','j','k','l','m','n','o','p',
+                                       'q','r','s','t','u','v','w','x',
+                                       'y','z','_'])
+def h : Regex := Union ((List.map Base ['a','b','c','d','e','f']) ++
+                        (List.map Base ['A','B','C','D','E','F']) ++
+                        (List.map Base ['0','1','2','3','4','5','6','7','8','9'] ))
+def e : Regex := Concat [Union [Base 'e', Base 'E'],
+                         qmark $ Union [Base '+', Base '-'],
+                         plus d]
+def fs : Regex := Union (List.map Base ['f','F','l','L'])
+def is : Regex := Star ∘ Union $ List.map Base ['u','U','l','L']
+
+-- return suffix of string after matching prefix with regex
+partial def regexConsume : Regex → String → List String
+  |      _          , "" => []
+  |     (.Base   c ), s  => if (c == s.front) then [String.mk s.toList.tail!] else []
+  |     (.Union  rs), s  => List.bind rs (λ r => regexConsume r s)
+  | rgx@(.Star   r ), s  => let rest := regexConsume r s
+                            (List.bind rest (regexConsume rgx)) ++ rest ++ [s]
+  |     (.Concat rs), s  => List.foldl (λ ss r => List.bind ss (regexConsume r)) [s] rs
+
+-- if the whole string is consumed, the regex matches
+def regexMatch (r : Regex) (s : String) : Bool := (regexConsume r s).elem ""
+
+-- example of hex numbers
+def c := Concat [Base '0', Union [Base 'x', Base 'X', empty], Concat [h, Star h], Union [empty, is]]
+#eval regexConsume c "0Xdead291l"
