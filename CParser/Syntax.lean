f618a21edc102d1536d5642034d0a515f0723567
Abhinav271828 <abhinav.menon@students.iiit.ac.in>
2022-07-14 22:16:14 +0530

    Fix imports

--- CParser/Syntax.lean
+++ CParser/Syntax.lean
@@ -2,3 +2,5 @@ import CParser.SyntaxDecl
 import CParser.Syntax.GroupOne
 import CParser.Syntax.GroupTwo
 import CParser.Syntax.GroupThree
+import CParser.Syntax.GroupFour
+import CParser.Syntax.GroupFive
