a189fb3a87909d1298a68282cdbdcd45024ad3cb
Andres Goens <andres.goens@ed.ac.uk>
2022-11-13 17:24:47 +0000

    Added a macro to parse an external .c file within Lean

--- CParser/Parser.lean
+++ CParser/Parser.lean
@@ -1,1 +1,10 @@
 import CParser.Parser.ParseFuncs
+
+elab "#parse " fileNameStx:str "as " cat:ident : command =>
+  open Lean Lean.Elab Command in
+  do
+     let filename := fileNameStx.getString
+     let fileStrIO <- IO.FS.readFile filename
+     let res := Parser.runParserCategory (← getEnv) cat.getId fileStrIO "<input>"
+     logInfo s!"{res}"
+
