8a7aec5e1a62f5a8c6e8a0b107d975b80a1342e2
Andres Goens <andres.goens@ed.ac.uk>
2022-11-15 16:59:28 +0000

    added proof of concept lexer hack with syntax extensions

---
+++ CParser/Extension.lean
@@ -0,0 +1,14 @@
+import Lean
+import CParser.SyntaxDecl
+open Lean
+
+macro "typedef" id:ident : command => do
+  let stratom : TSyntax `str := ⟨Syntax.mkStrLit id.getId.toString⟩
+  let stxstx : Array (TSyntax `stx) := #[( ← `(stx| $stratom:str)) ]
+  let cat := mkIdentFrom id `type_name_token
+  let stxDecl ← `(syntax  $[$stxstx]* : $cat)
+  return mkNullNode #[stxDecl]
+
+#check `(type_name_token| foo)
+typedef foo
+#check `(type_name_token| foo)
