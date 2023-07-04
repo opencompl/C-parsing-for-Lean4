eddde231cf373e4e70377219411c199e1847a9b4
Abhinav271828 <abhinav.m@research.iiit.ac.in>
2023-07-04 15:11:24 +0100

    Nuke old files

--- newWhitespace.lean
+++
@@ -1,9 +0,0 @@
-import Lean.Parser.Types
-
-/- Replace the functions (of the same names) 
-   with the following definitions in
-   lean4/src/Lean/Parser/Basic.lean -/
-
-import Lean
-open Lean.Parser
-
