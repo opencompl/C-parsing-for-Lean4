135bdb77837e6acbf6424257ea8fbf589610df7a
jatinAgarwala <jatin.a@research.iiit.ac.in>
2023-08-03 16:21:21 +0100

    Current MWE for char issue

--- CParser/AST/ClassDec.lean
+++ CParser/AST/ClassDec.lean
@@ -1,19 +1,179 @@
 import CParser.AST.AST
 
 open AST
-instance : Inhabited PrimaryExpr where default := PrimaryExpr.StringLit "defaultPE" 
-instance : Inhabited TranslUnit where default := TranslUnit.PrimaryExpr (default : PrimaryExpr)
+instance : Inhabited PrimaryExpr where default := PrimaryExpr.Constant 0
+-- instance : Inhabited PostfixExpr where default := PostfixExpr.Primary (default : PrimaryExpr)
+-- instance : Inhabited CastExpr where default := CastExpr.Primary (default : PrimaryExpr)
+-- instance : Inhabited MultExpr where default := MultExpr.Cast (default : CastExpr)
+-- instance : Inhabited AddExpr where default := AddExpr.Mult (default : MultExpr)
+-- instance : Inhabited ShiftExpr where default := ShiftExpr.Add (default : AddExpr)
+-- instance : Inhabited RelExpr where default := RelExpr.Shift (default : ShiftExpr)
+-- instance : Inhabited EqExpr where default := EqExpr.Rel (default : RelExpr)
+-- instance : Inhabited AndExpr where default := AndExpr.Eq (default : EqExpr)
+-- instance : Inhabited XOrExpr where default := XOrExpr.And (default : AndExpr)
+-- instance : Inhabited IOrExpr where default := IOrExpr.XOr (default : XOrExpr)
+-- instance : Inhabited LAndExpr where default := LAndExpr.IOr (default : IOrExpr)
+-- instance : Inhabited LOrExpr where default := LOrExpr.LAnd (default : LAndExpr)
+-- instance : Inhabited CondExpr where default := CondExpr.Primary (default : PrimaryExpr)
+-- instance : Inhabited AssmtOp where default := AssmtOp.Assign
+-- instance : Inhabited AssmtExpr where default := AssmtExpr.Primary (default : PrimaryExpr)
+-- instance : Inhabited ArgExprList where default := ArgExprList.AssmtExprList []
+-- instance : Inhabited Expression where default := Expression.Primary (default : PrimaryExpr)
+
+-- instance : Inhabited ConstantExpr where default := ConstantExpr.Primary (default : PrimaryExpr)
+-- instance : Inhabited DirAbstrDecl where default := DirAbstrDecl.DirAbDecConSqr (default : ConstantExpr)
+-- instance : Inhabited AbstrDecl where default := AbstrDecl.Primary (default : PrimaryExpr)
+instance : Inhabited IdentList where default := IdentList.IdentList []
+-- instance : Inhabited DirDecl where default := DirDecl.Identifier "foo"
+instance : Inhabited TypeQual where default := TypeQual.Const
+instance : Inhabited TypeQualList where default := TypeQualList.TypeQualList []
+instance : Inhabited Pointer where default := Pointer.StarTypeQualList (default : TypeQualList)
+-- instance : Inhabited Declarator where default := Declarator.DirDecl (default : DirDecl)
+instance : Inhabited InitList where default := InitList.InitList []
+instance : Inhabited Initializer where default := Initializer.InitListCurl (default : InitList)
+-- instance : Inhabited InitDecl where default := InitDecl.Declarator (default : Declarator)
+
+instance : Inhabited StorClassSpec where default := StorClassSpec.TypeDef
+instance : Inhabited DeclSpec where default := DeclSpec.StorClassSpec (default : StorClassSpec)
+instance : Inhabited Declaration where default := Declaration.Primary (default : PrimaryExpr)
+instance : Inhabited DeclList where default := DeclList.DeclList []
+-- instance : Inhabited Enumerator where default := Enumerator.Ident "foo"     -- Default for Identifier?
+-- instance : Inhabited EnumList where default := EnumList.EnumList []
+instance : Inhabited EnumSpec where default := EnumSpec.Primary (default : PrimaryExpr)
+-- instance : Inhabited InitDeclList where default := InitDeclList.InitDeclList []
+instance : Inhabited ParamDecl where default := ParamDecl.Primary (default : PrimaryExpr)
+instance : Inhabited ParamList where default := ParamList.ParamDecl (default : ParamDecl)
+instance : Inhabited ParamList where default := ParamList.ParamList []
+instance : Inhabited ParamTypeList where default := ParamTypeList.ParamList (default : ParamList)
+instance : Inhabited TypeSpec where default := TypeSpec.Void
+instance : Inhabited SpecQual where default := SpecQual.TypeSpec (default : TypeSpec)
+instance : Inhabited SpecQualList where default := SpecQualList.SpecQualList []
+-- instance : Inhabited StructDecl where default := StructDecl.Dec (default : Declarator)
+-- instance : Inhabited StructDeclList where default := StructDeclList.StructDeclList []
+-- instance : Inhabited StructDeclaration where default := StructDeclaration.SpecQualListStructDecList (default : SpecQualList) (default : StructDeclList)
+-- instance : Inhabited StructDeclarationList where default := StructDeclarationList.StructDeclarationList []
+-- instance : Inhabited StructOrUnion where default := StructOrUnion.Struct
+-- instance : Inhabited StructOrUnionSpec where default := StructOrUnionSpec.SoUStructDeclarationList (default : StructOrUnion) (default : StructDeclarationList)
+-- instance : Inhabited TypeName where default := TypeName.SpecQualList (default : SpecQualList)
+
+instance : Inhabited CompStmt where default := CompStmt.Brackets
+instance : Inhabited Statement where default := Statement.CompStmt (default : CompStmt)
+-- instance : Inhabited ExprStmt where default := ExprStmt.Semicolon
+-- instance : Inhabited SelStmt where default := SelStmt.If (default : Expression) (default : Statement)
+-- instance : Inhabited IterStmt where default := IterStmt.While (default : Expression) (default : Statement)
+-- instance : Inhabited JumpStmt where default := JumpStmt.Continue
+-- instance : Inhabited LabelStmt where default := LabelStmt.Default (default : Statement)
+instance : Inhabited StmtList where default := StmtList.StmtList []
+-- instance : Inhabited FuncDef where default := FuncDef.DeclCompStmt (default : Declarator) (default : CompStmt)
+instance : Inhabited ExternDecl where default := ExternDecl.Semicolon
+instance : Inhabited TranslUnit where default := TranslUnit.ExternDeclList []
 
 mutual
 partial def primaryExprToString : PrimaryExpr → String
   | .Identifier s => s
+  | .Constant c => toString c
+  | .FloatConstant c => toString c
   | .CharLit c => "'" ++ toString c ++ "'"
   | .StringLit s => "\"" ++ s ++ "\""
 
+partial def identListToString : IdentList → String
+  | .IdentList idents => " , ".intercalate (idents)
+
+partial def tqlToString : TypeQualList → String
+  | .TypeQualList tqs => " ".intercalate (tqs.map typeQualToString)
+
+partial def typeQualToString : TypeQual → String
+  | .Const => "const"
+  | .Volatile => "volatile"
+
+partial def pointerToString : Pointer → String
+  | .Star => "*"
+  | .StarTypeQualList tql => "* " ++ (tqlToString tql)
+  | .StarPtr p => "* " ++ (pointerToString p)
+  | .StarTypeQualListPtr tql p => "* " ++ (tqlToString tql) ++ " " ++ (pointerToString p)
+
+partial def initListToString : InitList → String
+  | .InitList inits => " , ".intercalate (inits.map initializerToString)
+
+partial def initializerToString : Initializer → String
+  | .InitListCurl il => "{" ++ (initListToString il) ++ "}"
+  | .InitListCurlComma il => "{" ++ (initListToString il) ++ ",}"
+
+partial def declarationToString : Declaration → String
+  | .Primary p => (primaryExprToString p)
+
+partial def declListToString : DeclList → String
+  | .DeclList ds => "\n".intercalate (ds.map declarationToString)
+
+partial def declSpecToString : DeclSpec → String
+  | .StorClassSpec d => (storClassSpecToString d)
+  | .StorClassSpecDeclSpec d i => (storClassSpecToString d) ++ " " ++ (declSpecToString i)
+  | .TypeSpec d => (typeSpecToString d)
+  | .TypeSpecDeclSpec d i => (typeSpecToString d) ++ " " ++ (declSpecToString i)
+  | .TypeQual d => (typeQualToString d)
+  | .TypeQualDeclSpec d i => (typeQualToString d) ++ " " ++ (declSpecToString i)
+
+partial def storClassSpecToString : StorClassSpec → String
+  | .TypeDef => "typedef"
+  | .Extern => "extern"
+  | .Static => "static"
+  | .Auto => "auto"
+  | .Register => "register"
+  | .Inline => "inline"
+
+partial def typeSpecToString : TypeSpec → String
+  | .Void => "void"
+  | .Char => "char"
+  -- | .Short => "short"
+  -- | .Int => "int"
+  -- | .Long => "long"
+  -- | .Float => "float"
+  -- | .Double => "double"
+  -- | .Signed => "signed"
+  -- | .Unsigned => "unsigned"
+  -- | .EnumSpec d => (enumSpecToString d)
+
+partial def enumSpecToString : EnumSpec → String
+  | .Primary p => (primaryExprToString p)
+
+partial def paramDeclToString : ParamDecl → String
+  | .Primary p => (primaryExprToString p)
+
+partial def paramListToString : ParamList → String
+  | .ParamList params => " , ".intercalate (params.map paramDeclToString)
+  | .ParamDecl d => (paramDeclToString d)
+  | .ParamListParamDecl d i => (paramListToString d) ++ " , " ++ (paramDeclToString i)
+
+partial def paramTypeListToString : ParamTypeList → String
+  | .ParamList d => (paramListToString d)
+  | .ParamListEllipsis d => (paramListToString d) ++ " , " ++ "..."
+
+partial def specQualToString : SpecQual → String
+  | .TypeSpec t => (typeSpecToString t)
+  | .TypeQual t => (typeQualToString t)
+
+partial def specQualListToString : SpecQualList → String
+  | .SpecQualList ts => " ".intercalate (ts.map specQualToString)
+
+partial def compStmtToString : CompStmt → String
+  | .Brackets => "{ }"
+  | .StmtList sl => "{\n" ++ (stmtListToString sl) ++ "\n}"
+  | .DeclList dl => "{\n" ++ (declListToString dl) ++ "\n}"
+  | .DeclListStmtList dl sl => "{\n" ++ (declListToString dl) ++ "\n\n" ++ (stmtListToString sl) ++ "\n}"
+
+partial def statementToString : Statement → String
+  | .CompStmt s => (compStmtToString s)
+
+partial def stmtListToString : StmtList → String
+  | .StmtList ss => "\n".intercalate (ss.map statementToString)
+
+partial def externDeclToString : ExternDecl → String
+  | .Declaration d => (declarationToString d)
+  | .Semicolon => ";"
+
 partial def translUnitToString : TranslUnit → String
-  | .PrimaryExpr p => primaryExprToString p
+  | .ExternDeclList eds => "\n".intercalate (eds.map externDeclToString)
 
 end
 
 instance : ToString PrimaryExpr where toString := primaryExprToString
-instance : ToString TranslUnit where toString := translUnitToString
--- CParser/Parser/MakeFuncs.lean
+++ CParser/Parser/MakeFuncs.lean
@@ -15,13 +15,150 @@ def getIdent (x : TSyntax [`ident, `type_name_token]) : Except String String :=
 mutual
 partial def mkPrimaryExpression : Lean.Syntax → Except String PrimaryExpr
   | `(primary_expression| $s:ident) => return (PrimaryExpr.Identifier s.getId.toString)
+  | `(primary_expression| $n:extended_num) => return PrimaryExpr.Constant (n.raw.getArg 0).toNat
+  | `(primary_expression| $s:scientific) => let ⟨a, b, c⟩ := s.getScientific
+                                            return PrimaryExpr.FloatConstant $ Float.ofScientific a b c 
   | `(primary_expression| $c:char) => return PrimaryExpr.CharLit c.getChar
   | `(primary_expression| $[$xs]*) => return PrimaryExpr.StringLit $ xs |>.map (λ stx => stx.getString)
                                                                         |>.foldl (λ s1 s2 => s1 ++ s2) ""
   | s => throw ("unexpected syntax for primary expression " ++ s!"{s}")
 
+partial def mkIdentList : Lean.Syntax → Except String IdentList
+  | `(identifier_list| $[$xs],*) =>
+     let is : Except String (Array String) := xs.mapM getIdent
+    IdentList.IdentList <$> (is.map Array.toList)
+  | s => throw ("unexpected syntax for identifier list " ++ s!"{s}")
+
+partial def mkTypeQualList : Lean.Syntax → Except String TypeQualList
+  | `(type_qualifier_list| $[$xs]*) => do
+      let tqs <- xs.mapM mkTypeQual
+      return TypeQualList.TypeQualList tqs.toList
+  | s => throw ("unexpected syntax for type qualifier list " ++ s!"{s}")
+
+partial def mkTypeQual : Lean.Syntax → Except String TypeQual
+  | `(type_qualifier| const) => return (TypeQual.Const)
+  | `(type_qualifier| volatile) => return (TypeQual.Volatile)
+  | s => throw ("unexpected syntax for type qualifier " ++ s!"{s}")
+
+partial def mkPointer : Lean.Syntax → Except String Pointer
+  | `(pointer| *) => return (Pointer.Star)
+  | `(pointer| * $t:type_qualifier_list) => Pointer.StarTypeQualList <$> (mkTypeQualList t)
+  | `(pointer| * $p:pointer) => Pointer.StarPtr <$> (mkPointer p)
+  | `(pointer| * $t:type_qualifier_list $p:pointer) => Pointer.StarTypeQualListPtr <$> (mkTypeQualList t) <*> (mkPointer p)
+  | s => throw ("unexpected syntax for pointer " ++ s!"{s}")
+
+partial def mkInitList : Lean.Syntax → Except String InitList
+  | `(initializer_list| $[$xs],*) => do
+      let inits <- xs.mapM mkInitializer
+      return InitList.InitList inits.toList
+  | s => throw ("unexpected syntax for initializer list " ++ s!"{s}")
+
+partial def mkInitializer : Lean.Syntax → Except String Initializer
+  | `(initializer| { $i:initializer_list }) => Initializer.InitListCurl <$> (mkInitList i)
+  | s => throw ("unexpected syntax for initializer " ++ s!"{s}")
+
+partial def mkDeclList : Lean.Syntax → Except String DeclList
+  | `(declaration_list| $d:declaration) => (λ d => DeclList.DeclList [d]) <$> (mkDeclaration d)
+  | `(declaration_list| $d:declaration $dl:declaration_list) => (λ (DeclList.DeclList dl) d => DeclList.DeclList ([d] ++ dl)) <$> (mkDeclList dl) <*> (mkDeclaration d)
+  | s => throw ("unexpected syntax for declaration list " ++ s!"{s}")
+
+partial def mkTypeSpec : Lean.Syntax → Except String TypeSpec
+  | `(type_specifier| void) => return TypeSpec.Void
+  | `(type_specifier| char) => return TypeSpec.Char
+  -- | `(type_specifier| short) => return TypeSpec.Short
+  -- | `(type_specifier| int) => return TypeSpec.Int
+  -- | `(type_specifier| long) => return TypeSpec.Long
+  -- | `(type_specifier| float) => return TypeSpec.Float
+  -- | `(type_specifier| double) => return TypeSpec.Double
+  -- | `(type_specifier| signed) => return TypeSpec.Signed
+  -- | `(type_specifier| unsigned) => return TypeSpec.Unsigned
+  -- | `(type_specifier| $e:enum_specifier) => TypeSpec.EnumSpec <$> (mkEnumSpec e)
+  | s => throw ("unexpected syntax for type specifier " ++ s!"{s}")
+
+partial def mkDeclSpec : Lean.Syntax → Except String DeclSpec
+  | `(declaration_specifiers| $s:storage_class_specifier) => DeclSpec.StorClassSpec <$> (mkStorClassSpec s)
+  | `(declaration_specifiers| $s:storage_class_specifier $d:declaration_specifiers) => DeclSpec.StorClassSpecDeclSpec <$> (mkStorClassSpec s) <*> (mkDeclSpec d)
+  | `(declaration_specifiers| $t:type_specifier) => DeclSpec.TypeSpec <$> (mkTypeSpec t)
+  | `(declaration_specifiers| $t:type_specifier $d:declaration_specifiers) => DeclSpec.TypeSpecDeclSpec <$> (mkTypeSpec t) <*> (mkDeclSpec d)
+  | `(declaration_specifiers| $t:type_qualifier) => DeclSpec.TypeQual <$> (mkTypeQual t)
+  | `(declaration_specifiers| $t:type_qualifier $d:declaration_specifiers) => DeclSpec.TypeQualDeclSpec <$> (mkTypeQual t) <*> (mkDeclSpec d)
+  | s => throw ("unexpected syntax for declaration specifier " ++ s!"{s}")
+
+partial def mkDeclaration : Lean.Syntax → Except String AST.Declaration
+  | `(declaration| $p:primary_expression) => Declaration.Primary <$> (mkPrimaryExpression p)
+  | s => throw ("unexpected syntax for declaration " ++ s!"{s}")
+
+partial def mkParamTypeList : Lean.Syntax → Except String ParamTypeList
+  | `(parameter_type_list| $pl:parameter_list ) => ParamTypeList.ParamList <$> (mkParamList pl)
+  | `(parameter_type_list| $pl:parameter_list , ... ) => ParamTypeList.ParamListEllipsis <$> (mkParamList pl)
+  | s => throw ("unexpected syntax for parameter type list " ++ s!"{s}")
+
+partial def mkParamDecl : Lean.Syntax → Except String ParamDecl
+  | `(parameter_declaration| $p:primary_expression) => ParamDecl.Primary <$> (mkPrimaryExpression p)
+  | s => throw ("unexpected syntax for parameter declaration " ++ s!"{s}")
+
+partial def mkParamList : Lean.Syntax → Except String ParamList
+  | `(parameter_list| $[$xs],*) => do
+      let params <- xs.mapM mkParamDecl
+      return ParamList.ParamList params.toList
+  | s => throw ("unexpected syntax for parameter list " ++ s!"{s}")
+
+partial def mkSpecQual : Lean.Syntax → Except String SpecQual
+  | `(specifier_qualifier| $t:type_specifier) => SpecQual.TypeSpec <$> (mkTypeSpec t)
+  | `(specifier_qualifier| $t:type_qualifier) => SpecQual.TypeQual <$> (mkTypeQual t)
+  | s => throw ("unexpected syntax for specifier qualifier " ++ s!"{s}")
+
+partial def mkSpecQualList : Lean.Syntax → Except String SpecQualList
+  | `(specifier_qualifier_list| $sq:specifier_qualifier) => (λ sq => SpecQualList.SpecQualList [sq]) <$> (mkSpecQual sq)
+  | `(specifier_qualifier_list| $sq:specifier_qualifier $sql:specifier_qualifier_list) => (λ sq (SpecQualList.SpecQualList sql) => SpecQualList.SpecQualList $ [sq] ++ sql) <$> (mkSpecQual sq) <*> (mkSpecQualList sql)
+  | s => throw ("unexpected syntax for specifier qualifier list " ++ s!"{s}")
+
+partial def mkStorClassSpec : Lean.Syntax → Except String StorClassSpec
+  | `(storage_class_specifier| typedef) => return StorClassSpec.TypeDef
+  | `(storage_class_specifier| extern) => return StorClassSpec.Extern
+  | `(storage_class_specifier| static) => return StorClassSpec.Static
+  | `(storage_class_specifier| auto) => return StorClassSpec.Auto
+  | `(storage_class_specifier| register) => return StorClassSpec.Register
+  | `(storage_class_specifier| inline) => return StorClassSpec.Inline
+  | s => throw ("unexpected syntax for storage class specifier " ++ s!"{s}")
+
+partial def mkEnumSpec : Lean.Syntax → Except String EnumSpec
+  | `(enum_specifier| $p:primary_expression) => EnumSpec.Primary <$> (mkPrimaryExpression p)
+  | s => throw ("unexpected syntax for enum specifier " ++ s!"{s}")
+
+partial def mkCompStmt : Lean.Syntax → Except String CompStmt
+  | `(compound_statement| { }) => return CompStmt.Brackets
+  | `(compound_statement| { $sl:statement_list }) => CompStmt.StmtList <$> (mkStmtList sl)
+  | `(compound_statement| { $dl:declaration_list }) => CompStmt.DeclList <$> (mkDeclList dl)
+  | `(compound_statement| { $dl:declaration_list $sl:statement_list }) => CompStmt.DeclListStmtList <$> (mkDeclList dl) <*> (mkStmtList sl)
+  | s => -- if s.getKind.toString == "choice"
+         -- then let args := s.getArgs
+         --      let c := args.find? (λ stx => stx.getKind.toString == "«compound_statement{__}»")
+         --      match c with
+         --        | .some stx => mkCompStmt stx
+         --        | .none => throw ("unexpected syntax for compound statement " ++ s!"{s}")
+         -- else throw ("unexpected syntax for compound statement " ++ s!"{s}")
+         throw ("unexpected syntax for unary expression " ++ s!"{s}")
+
+partial def mkStatement : Lean.Syntax → Except String Statement
+  | `(statement| $c:compound_statement) => Statement.CompStmt <$> (mkCompStmt c)
+  | s => throw ("unexpected syntax for statement " ++ s!"{s}")
+
+partial def mkStmtList : Lean.Syntax → Except String StmtList
+  | `(statement_list| $s:statement) => (λ s => StmtList.StmtList [s]) <$> mkStatement s
+  | `(statement_list| $s:statement $sl:statement_list) =>
+     (λ (StmtList.StmtList sl) li  => StmtList.StmtList ([li] ++ sl)) <$> (mkStmtList sl) <*> (mkStatement s)
+  | s => throw ("unexpected syntax for statement list " ++ s!"{s}")
+
+partial def mkExternDecl : Lean.Syntax → Except String ExternDecl
+  | `(external_declaration| $d:declaration) => ExternDecl.Declaration <$> (mkDeclaration d)
+  | `(external_declaration| ;) => return ExternDecl.Semicolon
+  | s => throw ("unexpected syntax for external declaration " ++ s!"{s}")
+
 partial def mkTranslUnit : Lean.Syntax → Except String TranslUnit
-  | `(translation_unit| $p:primary_expression) => TranslUnit.PrimaryExpr <$> (mkPrimaryExpression p)
+  | `(translation_unit| $[$xs]*) => do
+      let es <- xs.mapM mkExternDecl
+      return TranslUnit.ExternDeclList es.toList
   | s => throw ("unexpected syntax for translation unit " ++ s!"{s}")
 
 end
