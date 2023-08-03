f653bac048a6d5a2f530b261b3d93eb99ce293bf
jatinAgarwala <jatin.a@research.iiit.ac.in>
2023-08-03 16:44:22 +0100

    Reduce current MWE further

--- CParser/AST/AST.lean
+++ CParser/AST/AST.lean
@@ -10,104 +10,12 @@ inductive PrimaryExpr where
   | CharLit : Char → PrimaryExpr
   | StringLit : String → PrimaryExpr
 
-inductive IdentList where 
-  | IdentList : List String → IdentList
-
-inductive TypeQualList where 
-  | TypeQualList : List TypeQual → TypeQualList
-
-inductive TypeQual where
-  | Const : TypeQual
-  | Volatile : TypeQual
-
-inductive Pointer where
-  | Star : Pointer
-  | StarTypeQualList : TypeQualList → Pointer
-  | StarPtr : Pointer → Pointer
-  | StarTypeQualListPtr : TypeQualList → Pointer → Pointer
-
-inductive InitList where
-  | InitList : List Initializer → InitList
-
-inductive Initializer where
-  | InitListCurl : InitList → Initializer
-  | InitListCurlComma : InitList → Initializer
-
-inductive Declaration where
-  | Primary : PrimaryExpr → Declaration
-
-inductive DeclList where
-  | DeclList : List Declaration → DeclList
-
-inductive DeclSpec where
-  | StorClassSpec : StorClassSpec → DeclSpec
-  | StorClassSpecDeclSpec : StorClassSpec → DeclSpec → DeclSpec
-  | TypeSpec : TypeSpec → DeclSpec
-  | TypeSpecDeclSpec : TypeSpec → DeclSpec → DeclSpec
-  | TypeQual : TypeQual → DeclSpec
-  | TypeQualDeclSpec : TypeQual → DeclSpec → DeclSpec
-
-inductive EnumSpec where
-  | Primary : PrimaryExpr → EnumSpec
-
-inductive ParamDecl where
-  | Primary : PrimaryExpr → ParamDecl
-
-inductive ParamList where
-  | ParamList : List ParamDecl → ParamList
-  | ParamDecl : ParamDecl → ParamList
-  | ParamListParamDecl : ParamList → ParamDecl → ParamList
-
-inductive ParamTypeList where
-  | ParamList : ParamList → ParamTypeList
-  | ParamListEllipsis : ParamList → ParamTypeList
-
-inductive SpecQual where
-  | TypeSpec : TypeSpec → SpecQual
-  | TypeQual : TypeQual → SpecQual
-
-inductive SpecQualList where
-  | SpecQualList : List SpecQual → SpecQualList
-
-inductive StorClassSpec where
-  | TypeDef : StorClassSpec
-  | Extern : StorClassSpec
-  | Static : StorClassSpec
-  | Auto : StorClassSpec
-  | Register : StorClassSpec
-  | Inline : StorClassSpec
-
--- inductive TypeName where
---   | SpecQualList : SpecQualList → TypeName
---   | SpecQualListAbsDec : SpecQualList → AbstrDecl → TypeName
-
 inductive TypeSpec where
   | Void : TypeSpec
   | Char : TypeSpec
-  -- | Short : TypeSpec
   -- | Int : TypeSpec
-  -- | Long : TypeSpec
-  -- | Float : TypeSpec
-  -- | Double : TypeSpec
-  -- | Signed : TypeSpec
-  -- | Unsigned : TypeSpec
-  -- | EnumSpec : EnumSpec → TypeSpec
-  -- | TypeName : String → TypeSpec
-
-inductive CompStmt where
-  | Brackets : CompStmt
-  | StmtList : StmtList → CompStmt
-  | DeclList : DeclList → CompStmt
-  | DeclListStmtList : DeclList → StmtList → CompStmt
-
-inductive Statement where
-  | CompStmt : CompStmt → Statement
-
-inductive StmtList where
-  | StmtList : List Statement → StmtList
 
 inductive ExternDecl where
-  | Declaration : Declaration → ExternDecl
   | Semicolon : ExternDecl
 
 inductive TranslUnit where
--- CParser/SyntaxDecl.lean
+++ CParser/SyntaxDecl.lean
@@ -21,66 +21,6 @@ open Lean.PrettyPrinter.Formatter
 
 declare_syntax_cat extended_num
 declare_syntax_cat primary_expression
--- declare_syntax_cat postfix_expression
-declare_syntax_cat argument_expression_list
--- declare_syntax_cat unary_expression
--- declare_syntax_cat cast_expression
--- declare_syntax_cat multiplicative_expression
--- declare_syntax_cat additive_expression
--- declare_syntax_cat shift_expression
--- declare_syntax_cat relational_expression
--- declare_syntax_cat equality_expression
--- declare_syntax_cat and_expression
--- declare_syntax_cat exclusive_or_expression
--- declare_syntax_cat inclusive_or_expression
--- declare_syntax_cat logical_and_expression
--- declare_syntax_cat logical_or_expression
--- declare_syntax_cat conditional_expression
--- declare_syntax_cat assignment_expression
--- declare_syntax_cat assignment_operator
--- declare_syntax_cat expression
--- declare_syntax_cat constant_expression
-declare_syntax_cat declaration
-declare_syntax_cat declaration_specifiers
--- declare_syntax_cat init_declarator_list
--- declare_syntax_cat init_declarator
-declare_syntax_cat storage_class_specifier
 declare_syntax_cat type_specifier
--- declare_syntax_cat struct_or_union_specifier
--- declare_syntax_cat struct_or_union
--- declare_syntax_cat struct_declaration_list
--- declare_syntax_cat struct_declaration
-declare_syntax_cat specifier_qualifier
-declare_syntax_cat specifier_qualifier_list
--- declare_syntax_cat struct_declarator_list
--- declare_syntax_cat struct_declarator
-declare_syntax_cat enum_specifier
-declare_syntax_cat enumerator_list
-declare_syntax_cat enumerator
-declare_syntax_cat type_qualifier
--- declare_syntax_cat declarator
-declare_syntax_cat direct_declarator
-declare_syntax_cat pointer
-declare_syntax_cat type_qualifier_list
-declare_syntax_cat parameter_type_list
-declare_syntax_cat parameter_list
-declare_syntax_cat parameter_declaration
-declare_syntax_cat identifier_list
--- declare_syntax_cat type_name
--- declare_syntax_cat type_name_token
--- declare_syntax_cat abstract_declarator
-declare_syntax_cat direct_abstract_declarator
-declare_syntax_cat initializer
-declare_syntax_cat initializer_list
-declare_syntax_cat statement
--- declare_syntax_cat labeled_statement
-declare_syntax_cat compound_statement
-declare_syntax_cat declaration_list
-declare_syntax_cat statement_list
--- declare_syntax_cat expression_statement
--- declare_syntax_cat selection_statement
--- declare_syntax_cat iteration_statement
--- declare_syntax_cat jump_statement
 declare_syntax_cat translation_unit
 declare_syntax_cat external_declaration
--- declare_syntax_cat function_definition
